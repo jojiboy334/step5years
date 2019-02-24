27952e14a72879cbf751159fd53e64d24106187c
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-02-24 21:23:12 +0200

    Update 1.cpp
    Сделали задание 2

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <iomanip> // для использования setw
 using namespace std;
-void main() {
+int main() {
 	setlocale(LC_ALL, "Russian");
-	double const fut = 7.481;
-	int gal;
-	cout << "Vvedite kol-vo galonov" << endl;
-	cin >> gal;
-	cout << "Obem v kubicheskih futah =" << gal / fut << endl;
+	long pop1 = 135, pop2 = 7290, pop3 = 11300, pop4 = 16200;
+	cout << setw(9) << "Год" << setw(12) << "Число" << endl
+		<< setw(9) << "1990" << setw(12) << pop1 << endl
+		<< setw(9) << "1991" << setw(12) << pop2 << endl
+		<< setw(9) << "1992" << setw(12) << pop3 << endl
+		<< setw(9) << "1993" << setw(12) << pop4 << endl;
+	return 0;
 	system("pause");
-		
 }
