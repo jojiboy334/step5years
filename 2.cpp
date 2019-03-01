cbf9de7c9055dcbea21c79e3b30391c655a55347
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 20:23:04 +0200

    Update 2.cpp
    задание

--- 2.cpp
+++ 2.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <iomanip> // äëÿ èñïîëüçîâàíèÿ setw
 using namespace std;
-int main() {
-	setlocale(LC_ALL, "Russian");
-	long pop1 = 135, pop2 = 7290, pop3 = 11300, pop4 = 16200;
-	cout << setw(9) << "Year" << setw(12) << "number" << endl
-		<< setw(9) << "1990" << setw(12) << pop1 << endl
-		<< setw(9) << "1991" << setw(12) << pop2 << endl
-		<< setw(9) << "1992" << setw(12) << pop3 << endl
-		<< setw(9) << "1993" << setw(12) << pop4 << endl;
-	return 0;
-	system("pause");
+void main() {
+    setlocale(LC_ALL, "Russian");
+    double const fut = 7.481;
+    int gal;
+    cout << "Vvedite kol-vo galonov" << endl;
+    cin >> gal;
+    cout << "Obem v kubicheskih futah =" << gal / fut << endl;
+    system("pause");
+
 }
