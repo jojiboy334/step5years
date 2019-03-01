3b7e2ac24abebb87aee72ef14b5a74dce4ccadc1
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 19:47:21 +0200

    Update 2.cpp
    фывыфв

--- 2.cpp
+++ 2.cpp
@@ -6,7 +6,7 @@ int main() {
 	long pop1 = 135, pop2 = 7290, pop3 = 11300, pop4 = 16200;
 	cout << setw(9) << "Year" << setw(12) << "number" << endl
 		<< setw(9) << "1990" << setw(12) << pop1 << endl
-		<< setw(9) << "1991" << setw(12) << pop2 << endl
+		<< setw(9) << "1991" << setw(12) << pop2asdsad << endl
 		<< setw(9) << "1992" << setw(12) << pop3 << endl
 		<< setw(9) << "1993" << setw(12) << pop4 << endl;
 	return 0;
