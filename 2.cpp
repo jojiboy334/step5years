d08fc464b372756fb2bff11b50a648bb2fa47cac
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 23:23:39 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
+	int a, b, c, d;
 
-    system("pause");
+	cout << "enter the numerator of the first fraction" << endl;
+	cin >> a;
+	cout << "enter the denominator of the first fraction" << endl;
+	cin >> b;
+	cout << "enter the numerator of the second fraction" << endl;
+	cin >> c;
+	cout << "enter the denominator of the second fraction" << endl;
+	cin >> d;
+	cout << "the sum of fractions is equal to " << (a*d + b * c) << "/" << (b*d) << endl;
+	system("pause");
 }
