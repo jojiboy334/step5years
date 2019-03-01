e6c2b26d245112f39058b127924d25ae91e482d0
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 00:40:48 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <iomanip>
+#include <iomanip> // для использования setw
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
-
-    system("pause");
+	cout << setiosflags(ios::left) << setw(9) << "Surname"<< setw(12) << "Name" << setw(12) << "Adress" << setw(12) << "City" << endl << "------------------------------------------------"<< endl <<
+		                           setw(9) << "Petrov" << setw(12) << "Vasiliy" <<  setw(12) << "asddsasd" << setw(12) << "Saint-Petersburg" << endl
+		                           << setw(9) << "Ivanov" << setw(12) << "Sergay" << setw(12) << "asdasdasd" << setw(12) << "Nahodka" << endl
+		                           << setw(9) << "Sidorov"<< setw(12) << "Ivan" << setw(12) << "asdasdasd" << setw(12) << "Kaliningrad" << endl;
+	system("pause");
+	return 0;
 }
