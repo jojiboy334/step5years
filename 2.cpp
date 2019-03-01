db9234f4dc3dc6a2500c2788be865240739ede0b
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 20:58:44 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <iomanip>
+#include <ctype.h>
 using namespace std;
+
 int main() {
-    setlocale(LC_ALL, "Russian");
-
-
-    system("pause");
+	setlocale(LC_ALL, "Russian");
+	char symbol;
+	cout << "Enter the symbol: " << "\n";
+	cin >> symbol;
+	if (!islower(symbol))
+		cout << "0" << endl;
+	else cout << "1" << endl;
+	system("pause");
 }
