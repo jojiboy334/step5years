929cc3127a65d2175e415b0a8f9ff73d592047ae
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 00:22:40 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
+	float a, b, c;
+	cout << "Enter the number of pounds" << "\n";
+	cin >> a;
+	cout << "Enter the number of shillings" << "\n";
+	cin >> b;
+	cout << "Enter the number of pence" << "\n";
+	cin >> c;
+	b = b+(a * 20);
+	c = c+(b * 12);
+	cout << "Decimal pounds: "<< c/240 << "\n";
 
 
-    system("pause");
+	system("pause");
 }
