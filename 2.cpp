ca164d858402b46d39cd6e55c56183f9cee50a40
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 20:43:57 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <iomanip> // äëÿ èñïîëüçîâàíèÿ setw
+#include <iomanip>
 using namespace std;
 int main() {
-	setlocale(LC_ALL, "Russian");
-	long pop1 = 135, pop2 = 7290, pop3 = 11300, pop4 = 16200;
-	cout << setw(9) << "Year" << setw(12) << "number" << endl
-		<< setw(9) << "1990" << setw(12) << pop1 << endl
-		<< setw(9) << "1991" << setw(12) << pop2 << endl
-		<< setw(9) << "1992" << setw(12) << pop3 << endl
-		<< setw(9) << "1993" << setw(12) << pop4 << endl;
-	return 0;
-	system("pause");
+    setlocale(LC_ALL, "Russian");
+
+
+    system("pause");
 }
