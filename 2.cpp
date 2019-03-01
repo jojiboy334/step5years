a2c7491c106834fd3c06fdde2b147250b61f782e
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 18:56:15 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <iomanip> // для использования setw
+#include <iomanip> // Г¤Г«Гї ГЁГ±ГЇГ®Г«ГјГ§Г®ГўГ Г­ГЁГї setw
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	long pop1 = 135, pop2 = 7290, pop3 = 11300, pop4 = 16200;
-	cout << setw(9) << "Год" << setw(12) << "Число" << endl
+	cout << setw(9) << "Year" << setw(12) << "number" << endl
 		<< setw(9) << "1990" << setw(12) << pop1 << endl
 		<< setw(9) << "1991" << setw(12) << pop2 << endl
 		<< setw(9) << "1992" << setw(12) << pop3 << endl
