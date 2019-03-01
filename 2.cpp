7c79732f41490a6d930d630bb58f947bb8f81840
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-01 21:19:47 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
+
 int main() {
-    setlocale(LC_ALL, "Russian");
-
-
-    system("pause");
+	setlocale(LC_ALL, "Russian");
+	const double funt = 1.487;
+	const double frank = 0.172;
+	const double reikhmark = 0.584;
+	const double japyena = 0.00955;
+	int dollar;
+	cout << "Enter sum in dollars: " << "\n";
+	cin >> dollar;
+	cout << "Sum in funts will be => " << dollar/funt << "\n";
+	cout << "Sum in franks will be => " << dollar/frank << "\n";
+	cout << "Sum in reikhmarks will be => " << dollar/reikhmark << "\n";
+	cout << "Sum in japyens will be => " << dollar/japyena << "\n";
+	system("pause");
 }
