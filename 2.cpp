92ea3dfe81dc88c3761d2d2b18c0e2935d565004
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 14:21:22 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-int main() {
-    setlocale(LC_ALL, "Russian");
-
-
-    system("pause");
+int main()
+{
+	double n1, n2, ans;
+	char oper, ch;
+	do {
+		cout << "\Enter the first operand, the operation, the second operand: " << "\n";
+		cin >> n1 >> oper >> n2;
+		switch (oper)
+		{
+		case '+':ans = n1 + n2; break;
+		case '-':ans = n1 - n2; break;
+		case '*':ans = n1 * n2; break;
+		case '/':ans = n1 / n2; break;
+		default:ans = 0;
+		}
+		cout << "Answer = " << ans << "\n";
+		cout << "\Continue? (Enter 'y' or 'n')?" <<"\n";
+		cin >> ch;
+	} while (ch != 'n');
+	return 0;
 }
