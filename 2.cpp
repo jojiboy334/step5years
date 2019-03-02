02c617ddf4bc564676bf18928a0e791762e9d268
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 13:54:20 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-int main() {
-    setlocale(LC_ALL, "Russian");
-
-
-    system("pause");
+int main(void)
+{
+	float decPound, colvoPen1, colvoPen2, promezhut1, shilling, promezhut2, pens;
+	cout << "Enter: ";
+	cin >> decPound;
+	colvoPen1 = decPound * 240;
+	decPound = static_cast<int>(decPound);
+	colvoPen2 = decPound * 240;
+	promezhut1 = colvoPen1 - colvoPen2;
+	promezhut1 = static_cast<int>(promezhut1);
+	shilling = promezhut1 / 12;
+	shilling = static_cast<int>(shilling);
+	promezhut2 = shilling * 12;
+	pens = promezhut1 - promezhut2;
+	cout << "Result : " << decPound << "." << shilling << "." << pens << endl;
+	system("Pause");
 }
