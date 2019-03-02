9fb95889cf1e42909a2830da83fe816f268da6cb
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 13:23:59 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -2,8 +2,18 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
 
+	setlocale(LC_ALL, "Russian");
+	unsigned long number;
+	cout << "Enter the number" << "\n";
+	cin >> number;
 
-    system("pause");
+	for (int j = 1; j <= 200; j++)
+	{
+		cout << setw(5) << j * number << " ";
+		if (j % 10 == 0)
+			cout << endl;
+	}
+	system("pause");
+	return 0;
 }
