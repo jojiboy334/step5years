594b952353be8e823a247df7b1cfd8419e068054
Niko <47867391+jojiboy334@users.noreply.github.com>
2019-03-02 13:41:49 +0200

    Update 2.cpp
    1

--- 2.cpp
+++ 2.cpp
@@ -2,8 +2,27 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
-
-
-    system("pause");
+	setlocale(LC_ALL, "Russian");
+	cout << "Press 1 to convert the Celsius scale to the Fahrenheit scale\nPress 2 to convert the Fahrenheit scale to the Celsius scale: " << "\n";
+	int n;
+	double f;
+		double c;
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+		cout << "Enter the Celsius temperature: " << "\n";
+		cin >> c;
+		cout << "Fahrenheit temperature will be "<< c*(9./5)+32 << "\n";
+		break;
+	case 2:
+		cout << "Enter the Fahrenheit temperature: " << "\n";
+		cin >> f;
+		cout << "Celsius temperature will be "<< (f-32)*5/9 << endl;
+		break;
+	default:
+		cout << "You entered an invalid number" << endl;
+		break;
+	}
+	system("pause");
 }
